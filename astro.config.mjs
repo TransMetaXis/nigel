724d9df9ed1b5c4347b22e1cd6c62dfc656175ca
TransMetaXis <librawake@gmail.com>
2025-03-30 21:04:28 -0400

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,8 +12,8 @@ import remarkMath from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
-  site: TransMetaXis.github.io,
-  base: nigel,
+  site: https://TransMetaXis.github.io,
+  base: /nigel,
   trailingSlash: "ignore",
   prefetch: {
     prefetchAll: true
