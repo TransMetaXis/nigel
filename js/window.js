cac40c52b1613332ab21564cd5383273e66bccce
TransMetaXis <librawake@gmail.com>
2025-04-02 04:19:36 -0400

    Update window.js

--- js/window.js
+++ js/window.js
@@ -1,5 +1,14 @@
-window.addEventListener('resize', function() {
+/*window.addEventListener('resize', function() {
     var canvas = document.getElementById('canvas');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-});
+});*/
+export const fitRectIntoContainer = (rectWidth, rectHeight, containerWidth, containerHeight) => {
+    const widthRatio = containerWidth / rectWidth; // ratio container width to rect width
+    const heightRatio = containerHeight / rectHeight; // ratio container height to rect height
+    const ratio = Math.min(widthRatio, heightRatio); // take the smaller ratio
+    return {
+        width: rectWidth * ratio,
+        height: rectHeight * ratio,
+    };
+};
