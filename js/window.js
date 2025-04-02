e249fcad6ef91a0addbb92bb7f9631f913007a90
TransMetaXis <librawake@gmail.com>
2025-04-02 03:53:30 -0400

    Create window.js

---
+++ js/window.js
@@ -0,0 +1,5 @@
+window.addEventListener('resize', function() {
+    var canvas = document.getElementById('canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
