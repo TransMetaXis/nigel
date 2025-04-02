6ed570d926842af264aea57c9f545cb8b440546b
TransMetaXis <librawake@gmail.com>
2025-04-02 04:22:36 -0400

    Update window.js

--- js/window.js
+++ js/window.js
@@ -2,7 +2,7 @@
     var canvas = document.getElementById('canvas');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-});*/
+});
 export const fitRectIntoContainer = (rectWidth, rectHeight, containerWidth, containerHeight) => {
     const widthRatio = containerWidth / rectWidth; // ratio container width to rect width
     const heightRatio = containerHeight / rectHeight; // ratio container height to rect height
