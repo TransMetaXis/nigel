156d32aee9d6957a7d6736f7997181b2fb20cb34
TransMetaXis <librawake@gmail.com>
2025-04-02 20:19:02 -0400

    Update window.js

--- js/window.js
+++ js/window.js
@@ -1,10 +1,6 @@
-// Selecting the iframe element
-var frame = document.getElementById("yourIframeId");
-
-// Adjusting the iframe height onload event
-frame.onload = function() {
-    // Set the height of the iframe as the height of the iframe content
-    frame.style.height = frame.contentWindow.document.body.scrollHeight + 'px';
-    // Set the width of the iframe as the width of the iframe content
-    frame.style.width = frame.contentWindow.document.body.scrollWidth + 'px';
+window.onload = function() {
+    var iframe = document.getElementById("yourIframeId");
+    var canvas = iframe.contentWindow.document.getElementById("yourCanvasId");
+    canvas.width = iframe.offsetWidth;
+    canvas.height = iframe.offsetHeight;
 }
