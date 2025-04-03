54c0e5479989d2233f750cebc4ac97cf9608da21
TransMetaXis <librawake@gmail.com>
2025-04-02 20:45:46 -0400

    Update window.js

--- js/window.js
+++ js/window.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+/*window.onload = function() {
     var iframe = document.getElementById("games");
     var canvas = iframe.contentWindow.document.getElementById("gamecanvas");
     canvas.width = iframe.offsetWidth;
